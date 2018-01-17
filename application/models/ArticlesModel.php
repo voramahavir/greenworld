c8cfeac24cc9849bec78e600f5ee4f6d8280dd7a
Ajaj Patel <pajaj93@gmail.com>
2018-01-17 20:44:19 +0530

    - Commit 17-01

--- application/models/ArticlesModel.php
+++ application/models/ArticlesModel.php
@@ -107,6 +107,7 @@ class ArticlesModel extends CI_Model {
         if(!empty($search)){$this->db->like("a.description",$search);}
         $this->db->select('id,description,image_url,url,title,source,submitted_by,designation,video_url,image_type,video_type,a.is_active,a.user_id,a.created_at,CONCAT(u.first_name, " ",u.last_name) as user_fullname,u.profile_pic as user_profile_pic');
         $this->db->join('users as u','u.user_id = a.user_id', 'left');
+        $this->db->order_by('a.created_at','DESC'); 
         $output['data'] = $this->db->get('articles as a')->result();
         if(!empty($search)){$this->db->like("a.description",$search);}
         // if($id>0){
